9d6ed3bfd3a928c1f9e05abce07c70f117cc7981
João Anderson <joao.anderson.af@gmail.com>
2025-10-02 16:45:00 -0300

    Criação de projeto Spring Boot

---
+++ apis/ambientes-internos/src/test/kotlin/br/edu/ifce/ambientes_internos/AmbientesInternosApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.edu.ifce.ambientes_internos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AmbientesInternosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
