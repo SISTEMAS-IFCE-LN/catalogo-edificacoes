8c47a1c07639930c18682154556dd5c32853b00a
João Anderson <joao.anderson.af@gmail.com>
2025-10-07 11:33:49 -0300

    Criando endpoind para testar aplicação spring boot

---
+++ apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/controller/HelloController.kt
@@ -0,0 +1,18 @@
+package br.edu.ifce.ambientes_internos.controller
+
+import br.edu.ifce.ambientes_internos.model.domain.Hello
+import br.edu.ifce.ambientes_internos.model.service.HelloService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello")
+class HelloController(val helloService: HelloService) {
+
+    @GetMapping
+    fun getHelloMessage(): Hello {
+        return helloService.getHelloMessage()
+    }
+
+}
---
+++ apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/domain/Hello.kt
@@ -0,0 +1,4 @@
+package br.edu.ifce.ambientes_internos.model.domain
+
+class Hello(var message: String = "Hello, World!" ) {
+}
