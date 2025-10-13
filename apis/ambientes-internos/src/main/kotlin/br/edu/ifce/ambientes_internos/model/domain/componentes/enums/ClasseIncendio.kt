56b2e85f6fbc90bfb60b63c9804033c966d07dad
João Anderson <joao.anderson.af@gmail.com>
2025-10-13 10:56:38 -0300

    Adicionando classe Extintor e relacionadas.

---
+++ apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/domain/componentes/enums/ClasseIncendio.kt
@@ -0,0 +1,9 @@
+package br.edu.ifce.ambientes_internos.model.domain.componentes.enums
+
+enum class ClasseIncendio(val nome: String) {
+    A("Classe A - Materiais Sólidos"),
+    B("Classe B - Líquidos Inflamáveis"),
+    C("Classe C - Equipamentos Elétricos"),
+    D("Classe D - Metais Combustíveis"),
+    K("Classe K - Óleos e Gorduras de Cozinha")
+}
---
+++ apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/domain/componentes/enums/TipoExtintor.kt
@@ -0,0 +1,8 @@
+package br.edu.ifce.ambientes_internos.model.domain.componentes.enums
+
+enum class TipoExtintor(val nome: String) {
+    AGUA("Água"),
+    CO2("Dióxido de Carbono (CO2)"),
+    PO_QUIMICO("Pó Químico"),
+    OUTRO("Outro")
+}
