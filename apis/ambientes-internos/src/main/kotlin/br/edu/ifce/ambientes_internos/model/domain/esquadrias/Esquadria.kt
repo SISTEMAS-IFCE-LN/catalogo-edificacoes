e0474f555b93aab1363b29308bc5d9271d458776
João Anderson <joao.anderson.af@gmail.com>
2025-10-22 15:06:41 -0300

    Implementando métodos de cálculo de área e comprimento na classe Esquadria

--- apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/domain/esquadrias/Esquadria.kt
+++ apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/domain/esquadrias/Esquadria.kt
@@ -2,6 +2,7 @@ package br.edu.ifce.ambientes_internos.model.domain.esquadrias
 
 import br.edu.ifce.ambientes_internos.model.domain.geometrias.Geometria
 import java.math.BigDecimal
+import java.math.RoundingMode
 
 abstract class Esquadria(
     var id: Long?,
@@ -11,19 +12,27 @@ abstract class Esquadria(
 ) {
 
     fun calcularAreaFolhasM2(): BigDecimal {
-        throw NotImplementedError("Metodo não implementado")
+        return componentes.filterIsInstance<Folha>()
+            .fold(BigDecimal.ZERO) { acc, folha -> acc.add(folha.geometria.calcularAreaTotalM2()) }
+            .setScale(2, RoundingMode.HALF_UP)
     }
 
     fun calcularAreaBandeirolasM2(): BigDecimal {
-        throw NotImplementedError("Metodo não implementado")
+        return componentes.filterIsInstance<Bandeirola>()
+            .fold(BigDecimal.ZERO) { acc, bandeirola -> acc.add(bandeirola.geometria.calcularAreaTotalM2()) }
+            .setScale(2, RoundingMode.HALF_UP)
     }
 
     fun calcularComprimentoGuarnicoesM(): BigDecimal {
-        throw NotImplementedError("Metodo não implementado")
+        return componentes.filterIsInstance<Guarnicao>()
+            .fold(BigDecimal.ZERO) { acc, guarnicao -> acc.add(guarnicao.geometria.altura.multiply(BigDecimal(guarnicao.geometria.repeticao))) }
+            .setScale(2, RoundingMode.HALF_UP)
     }
 
     fun calcularAreaGuarnicoesM2(): BigDecimal {
-        throw NotImplementedError("Metodo não implementado")
+        return componentes.filterIsInstance<Guarnicao>()
+            .fold(BigDecimal.ZERO) { acc, guarnicao -> acc.add(guarnicao.geometria.calcularAreaTotalM2()) }
+            .setScale(2, RoundingMode.HALF_UP)
     }
 
 }
