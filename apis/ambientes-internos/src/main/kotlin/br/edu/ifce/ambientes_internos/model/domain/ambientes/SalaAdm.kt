74ffefff9b6a4865ce29ee0517dadd7f87e0d2cf
João Anderson <joao.anderson.af@gmail.com>
2025-10-24 14:59:24 -0300

    Adicionando classes Auditorio, SalaAdm e SalaAula para representar primeiros tipos de ambientes.

---
+++ apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/domain/ambientes/SalaAdm.kt
@@ -0,0 +1,20 @@
+package br.edu.ifce.ambientes_internos.model.domain.ambientes
+
+import br.edu.ifce.ambientes_internos.model.domain.componentes.Componente
+import br.edu.ifce.ambientes_internos.model.domain.elementos_construtivos.Parede
+import br.edu.ifce.ambientes_internos.model.domain.elementos_construtivos.Piso
+import br.edu.ifce.ambientes_internos.model.domain.elementos_construtivos.Teto
+
+class SalaAdm(
+    nome: String,
+    localizacao: String,
+    capacidade: Int,
+    pisos: MutableList<Piso> = mutableListOf(),
+    paredes: MutableList<Parede> = mutableListOf(),
+    tetos: MutableList<Teto> = mutableListOf(),
+    componentes: MutableList<Componente> = mutableListOf(),
+    publicado: Boolean = false,
+    informacaoAdicional: String = "",
+    id: Long? = null
+): Ambiente(id, nome, localizacao, capacidade, informacaoAdicional, publicado, pisos, paredes, tetos, componentes) {
+}
