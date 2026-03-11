ae479b01a516be5d73efe0a8127d2b31c6b69a46
João Anderson <joao.anderson.af@gmail.com>
2026-03-11 09:23:00 -0300

    Adicionando totalTipoMaterial à resposta de ambientes paginados, calculando a área das esquadrias por tipo e material.

--- apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/dto/esquadria/EsquadriasAmbientesPaginadosRes.kt
+++ apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/dto/esquadria/EsquadriasAmbientesPaginadosRes.kt
@@ -5,9 +5,11 @@ import br.edu.ifce.ambientes_internos.model.dto.DadosPaginacao
 import br.edu.ifce.ambientes_internos.model.dto.ambiente.AmbienteNomeLocalizacaoRes
 import br.edu.ifce.ambientes_internos.model.dto.ambiente.LocalizacaoRes
 import org.springframework.data.domain.Page
+import java.math.BigDecimal
 
 data class EsquadriasAmbientesPaginadosRes(
     val ambientes: Map<AmbienteNomeLocalizacaoRes, EsquadriasDetalhesRes>,
+    val totalTipoMaterial: List<EsquadriaTipoMaterialRes>,
     val dadosPaginacao: DadosPaginacao
 ) {
     companion object {
@@ -19,8 +21,17 @@ data class EsquadriasAmbientesPaginadosRes(
                     localizacao = LocalizacaoRes.from(ambiente.localizacao)
                 ) to EsquadriasDetalhesRes.from(ambiente)
             }
+            val totalTipoMaterial = page.content.flatMap { ambiente ->
+                ambiente.calcularAreaEsquadriasPorTipoMaterial().map { EsquadriaTipoMaterialRes.from(it) }
+            }.groupBy { Pair(it.tipo, it.material) }
+                .mapValues { entry ->
+                    entry.value.fold(BigDecimal.ZERO) { acc, res ->
+                        acc.add(res.area)
+                    }
+                }.map { EsquadriaTipoMaterialRes.from(it) }
             return EsquadriasAmbientesPaginadosRes(
                 ambientes = ambientes,
+                totalTipoMaterial = totalTipoMaterial,
                 dadosPaginacao = DadosPaginacao.from(page)
             )
         }
