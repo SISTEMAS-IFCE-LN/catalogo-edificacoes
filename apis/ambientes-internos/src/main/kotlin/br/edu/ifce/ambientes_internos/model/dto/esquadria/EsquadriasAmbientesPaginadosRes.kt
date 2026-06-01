14d27c9dc5f4f487deb941b024d7894bf2502cde
João Anderson <joao.anderson.af@gmail.com>
2026-06-01 10:21:49 -0300

    Alterando a estrutura de dados de ambientes para utilizar uma lista de pares.

--- apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/dto/esquadria/EsquadriasAmbientesPaginadosRes.kt
+++ apis/ambientes-internos/src/main/kotlin/br/edu/ifce/ambientes_internos/model/dto/esquadria/EsquadriasAmbientesPaginadosRes.kt
@@ -8,18 +8,20 @@ import org.springframework.data.domain.Page
 import java.math.BigDecimal
 
 data class EsquadriasAmbientesPaginadosRes(
-    val ambientes: Map<AmbienteNomeLocalizacaoRes, EsquadriasDetalhesRes>,
+    val ambientes: List<Pair<AmbienteNomeLocalizacaoRes, EsquadriasDetalhesRes>>,
     val totalTipoMaterial: List<EsquadriaTipoMaterialRes>,
     val dadosPaginacao: DadosPaginacao
 ) {
     companion object {
         fun from(page: Page<Ambiente>): EsquadriasAmbientesPaginadosRes {
-            val ambientes = page.content.associate { ambiente ->
-                AmbienteNomeLocalizacaoRes(
-                    id = ambiente.id!!,
-                    nome = ambiente.nome,
-                    localizacao = LocalizacaoRes.from(ambiente.localizacao)
-                ) to EsquadriasDetalhesRes.from(ambiente)
+            val ambientes = page.content.map { ambiente ->
+                Pair(
+                    AmbienteNomeLocalizacaoRes(
+                        id = ambiente.id!!,
+                        nome = ambiente.nome,
+                        localizacao = LocalizacaoRes.from(ambiente.localizacao)
+                    ), EsquadriasDetalhesRes.from(ambiente)
+                )
             }
             val totalTipoMaterial = page.content.flatMap { ambiente ->
                 ambiente.calcularAreaEsquadriasPorTipoMaterial().map { EsquadriaTipoMaterialRes.from(it) }
