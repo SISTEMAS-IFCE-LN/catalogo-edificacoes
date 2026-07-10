4892bd7c5536b1b82ae4aab0f3c4cfd3f2949a1d
João Anderson <joao.anderson.af@gmail.com>
2026-07-10 10:48:21 -0300

    Criando um healthcheck para a api

---
+++ apis/HealthCheck.java
@@ -0,0 +1,15 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HealthCheck {
+    public static void main(String[] args) {
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL("http://localhost:8080/actuator/health").openConnection();
+            conn.setConnectTimeout(3000);
+            conn.setReadTimeout(3000);
+            System.exit(conn.getResponseCode() < 400 ? 0 : 1);
+        } catch (Exception e) {
+            System.exit(1);
+        }
+    }
+}
